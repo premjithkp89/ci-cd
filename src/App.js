5a162c47e454a2036f260d07a786b42cf27f6af4
Premjith <ust-premjith.prasannan@tunstall.com>
2021-07-12 16:13:46 +0530

    title updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         "TEstssddd"
-        </p>
+        <p>"Travis added"</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-      
-        </a>
+        ></a>
       </header>
     </div>
   );
