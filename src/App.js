5ee8a1f44adafb41e8886b56a11fb157534832fe
Premjith <ust-premjith.prasannan@tunstall.com>
2021-07-12 16:17:26 +0530

    title updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>"Travis added"</p>
+        <p>"Travis added ...."</p>
         <a
           className="App-link"
           href="https://reactjs.org"
